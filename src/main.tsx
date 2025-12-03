3c38fc15e8f1708d903be51828dbb288aa78317a
28 Sadjuo <28sadjuo@gmail.com>
2025-12-03 05:05:35 +0200

    Fix main.tsx – correct React render (final blank page fix)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'                     // ← THIS LINE WAS MISSING BEFORE!
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './contexts/AuthContext.tsx'
-import { ThemeProvider } from './contexts/ThemeContext.tsx'
-
-const queryClient = new QueryClient()
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </AuthProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
